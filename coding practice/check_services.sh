daf74e35c2ad976456a905c5ac3b2d2ed4d7b6af
Shiv <smrathod291@gmail.com>
2026-07-07 17:40:04 +0000

    complted todays task

---
+++ coding practice/check_services.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+services=("nginx" "docker" "apache2")
+
+for service in "${services[@]}"
+do
+    if systemctl is-active --quiet "$service"; then
+        echo "$service is running"
+    else
+        echo "$service is not running"
+    fi
+done
